69f4d794c010badf30127f1fb38c5b8655b4627e
napak <napaks@gmail.com>
2018-12-15 10:40:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+    res.write('1. You have to group the below words by having the same letter but different position\n');
+    res.write(groupWords());
+    res.write('\n');
+    res.write('2. You have to remove the parentheses and reverse the word inside.\n');
+    res.write(reverseRemoveParen());
+    res.end();
+
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const words = ['AMOR', 'XISELA', 'JAMON', 'ROMA', 'OMAR', 
+            'MORA', 'ESPONJA', 'RAMO', 'JAPONES', 'ARMO', 
+            'MOJAN', 'MARO', 'ORAM', 'MONJA', 'ALEXIS'];
+
+function groupWords() { 
+    var output = "";
+
+    var groupBy = function(xs, key) {
+        return xs.reduce(function(rv, x) {
+          (rv[x[key]] = rv[x[key]] || []).push(x);
+          return rv;
+        }, {});
+      };
+
+    var wordToGroups = [];
+
+    words.forEach(function(value) {
+        wordToGroups.push({ length: value.length, value: value });
+    })
+
+    var groupedWords = groupBy(wordToGroups, 'length');
+
+    for (var key in groupedWords) {
+        var arr = groupedWords[key];
+
+        var text = "";
+        for( var i = 0; i < arr.length; i++ ) {
+            var obj = arr[ i ];
+
+            if (text.length == 0) {
+                text = obj['value'];  
+            } else {
+                text = text + ' - ' + obj['value'];
+            }
+        }
+
+        output += text + '\n';
+     }
+
+    return output; 
+} 
+
+function reverseRemoveParen() {
+    var array;
+    var reversString = function (str) {
+        array = str.replace("(","").replace(")","").split("");
+        array.reverse();
+        str = array.join('');
+        return str.replace("(","").replace(")","");
+    }
+
+    var output = "";
+    const fooWords = ['foo(bar)','(bar)','foo(bar)blim','foo(foo(bar))blim'];
+
+    fooWords.forEach(function(value) {
+        var start = value.indexOf('(');
+        var end = value.lastIndexOf(')') + 1;
+        var str = value.substr(start, end - start);
+        console.log(str);
+        var rvStr = reversString(str);
+
+        //console.log(value + ' => ' + value.replace(str, rvStr));
+        output += value + ' => ' + value.replace(str, rvStr) + '\n';
+    })
+
+    return output;
+}
+
